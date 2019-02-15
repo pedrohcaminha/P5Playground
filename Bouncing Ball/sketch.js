08dc0efa9e911690c4b7c227e63e700623515e87
pedrohcaminha <pedrorecife@gmail.com>
2019-02-15 13:31:26 -0300

    Add earthquake

---
+++ Bouncing Ball/sketch.js
@@ -0,0 +1,36 @@
+function preload() {
+	soundFormats('mp3');
+	bounce = loadSound('bounce.mp3');
+  }
+function setup() {
+	bounce.setVolume(0.5);
+	createCanvas(windowWidth, windowHeight);
+	
+}
+var x = 0;
+var y = 30;
+var aceleracao = 1.09;
+var a = 5;
+var cor = 0;
+function draw() {
+		var color = map(y, 0, windowWidth, 0, 255);
+		background(color);
+		fill (cor, 0,0);
+		ellipse (x = x+a, y, 30, 30);
+		
+		
+		y = y * aceleracao;
+		if(x >= windowWidth | x<= 0){
+			a = -a;
+			
+			bounce.play();
+		}
+		cor = map(y, windowHeight, 0, 0, 255);
+		console.log(x);
+		if(y > windowHeight-15 | y < 29){
+			if(y>100)
+			bounce.play();
+			aceleracao = 1/aceleracao;
+		}
+	
+	}
---
+++ Table View/sketch.js
@@ -0,0 +1,50 @@
+let r = 400;
+let times =500;
+let mult = 0;
+let tax = 0.001;
+let red = 0;
+let green = 0;
+let blue = 0;
+let rep = 0;
+
+function setup() {
+	// frameRate(10);
+	createCanvas(r*2,r*2);
+	background(0);
+	fill(255);
+	ellipse(r, r, r*2, r*2);
+}
+
+function draw(){
+	background(0);
+	fill(255);
+	textSize(20);
+	text(mult-mult%1, 10, 30);
+	ellipse(r, r, r*2, r*2);
+	stroke(red,green,blue);
+	let x = mult;
+	for(let y = 0; y < times; y+=1){
+		let angle1 = map(y, 0, times, 0, TWO_PI);
+		let angle2 = map((y*mult)%times, 0, times, 0, TWO_PI);
+		let x1 = r*cos(angle1) + r;
+		let y1 = r*sin(angle1) + r;
+		let x2 = r*cos(angle2) + r;
+		let y2 = r*sin(angle2) + r;
+		line(x1, y1, x2, y2);
+	}
+	if(mouseIsPressed){
+		mult = mult;
+	}else{
+		mult+=tax;
+	}
+	rep++;
+	if(rep == 1/tax){
+		red = random(0, 100);
+		green = random(0, 100);
+		blue = random(0, 100);
+		rep = 0;
+		saveFrames('out', 'png', 1, 1, data => {
+			print(data);
+		  });
+	}
+}
